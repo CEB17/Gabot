fdc1880da3e0a1d9a39c080dba94c0d8255fe6f1
KY64 <taurus99@protonmail.com>
2020-01-26 06:55:24 +0700

    Set debug mode to True

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-d', '--debug', default=False, help='debug')
+    arg_parser.add_argument('-d', '--debug', default=True, help='debug')
     options = arg_parser.parse_args()
 
     app.run(threaded=True, debug=options.debug, port=os.getenv('PORT', None))
