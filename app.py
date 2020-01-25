4ecda68ea82efbaf910abd07d27d0771e55937d1
KY64 <taurus99@protonmail.com>
2020-01-26 00:30:19 +0700

    change port

--- app.py
+++ app.py
@@ -30,8 +30,7 @@ if __name__ == "__main__":
     arg_parser = ArgumentParser(
         usage='Usage: python ' + __file__ + ' [--port <port>] [--help]'
     )
-    arg_parser.add_argument('-p', '--port', type=int, default=os.getenv('PORT', None), help='port')
     arg_parser.add_argument('-d', '--debug', default=False, help='debug')
     options = arg_parser.parse_args()
 
-    app.run(debug=options.debug, port=options.port)
+    app.run(threaded=True, debug=options.debug, port=os.getenv('PORT', None))
