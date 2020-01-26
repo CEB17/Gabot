7777382756bb9617191fa5fe5891ad743a17fc43
KY64 <taurus99@protonmail.com>
2020-01-26 14:57:19 +0700

    Debugging #5

--- event/message.py
+++ event/message.py
@@ -1,9 +1,9 @@
 from linebot.models import (
     TextMessage
 )
-from res import (text)
+from res.text import *
 
 class MessageHandler():
     def __init__(self, event, line_bot_api):
         if isinstance(event.message, TextMessage):
-            text.greeting.greet(event, line_bot_api)
+            Greet(event, line_bot_api)
--- res/text/greeting.py
+++ res/text/greeting.py
@@ -2,14 +2,21 @@ from linebot.models import (
     TextSendMessage,
 )
 
-def greet(event, line_bot_api):
-    if event.message.text == "Cuy!":
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text="Apa cuy???")
-        )
-    else:
-        line_bot_api.reply_message(
-            event.reply_token,
-            TextSendMessage(text=event.message.text)
-        )
+class Greet():
+    def __init__(self, event, line_bot_api):
+        self.event = event
+        self.line_bot_api = line_bot_api
+        
+        self.greet()
+
+    def greet(self):
+        if self.event.message.text == "Cuy!":
+            self.line_bot_api.reply_message(
+                self.event.reply_token,
+                TextSendMessage(text="Apa cuy???")
+            )
+        else:
+            self.line_bot_api.reply_message(
+                self.event.reply_token,
+                TextSendMessage(text=self.event.message.text)
+            )
