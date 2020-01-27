f2a8b29816dd4c4212fe2734d786fec4112263c3
KY64 <taurus99@protonmail.com>
2020-01-27 22:04:00 +0700

    Debugging #2

--- res/text/mention.py
+++ res/text/mention.py
@@ -10,4 +10,5 @@ class Mention():
         self.line_bot_api = line_bot_api
 
     def mentionAll(self):
-        members = self.line_bot_api.get_group_member_ids("uy")
+        members = self.line_bot_api.get_group_member_ids("Ca8f1ae3f2b72fc2da7345f4314dd57f0")
+        print(members)
