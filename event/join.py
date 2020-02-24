8885b7af9da1e9323ddf75c2981b195830559707
ky64 <taurus99@protonmail.com>
2020-02-24 01:56:45 -0500

    Debugging

--- event/join.py
+++ event/join.py
@@ -8,28 +8,28 @@ class JoinHandler():
         self.event = event
         self.line_bot_api = line_bot_api
 
-        if event.source.group_id != os.getenv('GROUP_ID', None):
+        if event.source.type == "group":
+            if event.source.group_id != os.getenv('GROUP_ID', None):
+                line_bot_api.push_message(
+                    os.getenv('ADMIN', None),
+                    TextSendMessage(
+                        f"Help!! 0x10001C I almost got kidnapped on {event.source.group_id}"
+                    )
+                )
+                line_bot_api.leave_group(event.source.group_id)
+                return
             line_bot_api.push_message(
                 os.getenv('ADMIN', None),
                 TextSendMessage(
-                    f"Help!!0x10001C I almost got kidnapped on {event.source.group_id}"
+                    f"I'm joining group {event.source.group_id}"
                 )
             )
-            line_bot_api.leave_group(event.source.group_id)
-            return
         elif event.source.type == "room":
             line_bot_api.push_message(
                 os.getenv('ADMIN', None),
                 TextSendMessage(
-                    f"Help!!0x10001C I almost got kidnapped on {event.source.room_id}"
+                    f"Help!! 0x10001C I almost got kidnapped on {event.source.room_id}"
                 )
             )
             line_bot_api.leave_room(event.source.room_id)
             return
-
-        line_bot_api.push_message(
-            os.getenv('ADMIN', None),
-            TextSendMessage(
-                f"I'm joining group {event.source.group_id}"
-            )
-        )
