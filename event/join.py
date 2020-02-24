7d462d6042bb61102d6af15203ec573dd9c96b62
ky64 <taurus99@protonmail.com>
2020-02-23 22:50:31 -0500

    Test join handler

--- event/join.py
+++ event/join.py
@@ -6,3 +6,6 @@ class JoinHandler():
     def __init__(self, event, line_bot_api):
         self.event = event
         self.line_bot_api = line_bot_api
+        member = self.line_bot_api.get_group_member_ids(event.source.group_id)
+        print(member.member_ids)
+        print(member.next)
